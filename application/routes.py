dad8a5ddc07eb7d1eb493f982daf50f814d2fbf4
tsangsimon <simon.tsang@gmail.com>
2015-07-15 17:39:46 +0100

    route to get register from currentregister table

--- application/routes.py
+++ application/routes.py
@@ -1,15 +1,16 @@
 from application import app
 from application import db
+import json
 
 @app.route("/health")
 def check_status():
     return "Status OK"
 
-@app.route("/register/<caseid>", methods=["GET"])
+@app.route("/register/<titlenumber>", methods=["GET"])
 def register(titlenumber):
     title = get_title_from_current_register(titlenumber)
 
-    return title, 200
+    return json.dumps(title)
 
 
 #gets the title from the current register.
@@ -17,17 +18,19 @@ def get_title_from_current_register(title_number):
     # Gets the version of title number with the latest ID on the table
     title = None
     if check_title_exists(title_number):
-        sql_text = "SELECT * FROM records WHERE registers ->> 'title_number' = '%s' order by id desc limit 1;" % title_number
+        sql_text = "SELECT * FROM registers WHERE record ->> 'title_number' = '%s' order by id desc limit 1;" % title_number
         result = db.engine.execute(sql_text)
+
         for row in result:
-            title = row['record']
+            title = row["record"]
+    # import pdb; pdb.set_trace()
 
     return title
 
 def check_title_exists(title_number):
     # Gets the version of title number with the latest ID on the table
     title_exists = False
-    sql_text = "SELECT 1 FROM records WHERE registers ->> 'title_number' = '%s' order by id desc limit 1;" % title_number
+    sql_text = "SELECT 1 FROM registers WHERE record ->> 'title_number' = '%s' order by id desc limit 1;" % title_number
     result = db.engine.execute(sql_text)
     for row in result:
         title_exists = True
