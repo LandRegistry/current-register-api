5719d9b754cde54ffe57573850b86660fd0827d1
Skablam <matthew.pease@landregistry.gsi.gov.uk>
2015-08-05 09:58:20 +0100

    enabled run.sh to be run from anywhere

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 export SETTINGS=config.DevelopmentConfig
 
+#get the absolute path to the directory that this script is in
+dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $dir
+
 python run.py
