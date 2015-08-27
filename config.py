2972852f5016e0abbd4a3f67d8f7bfe8f346a345
Andy Warren <andrew.warren@landregistry.gsi.gov.uk>
2015-08-27 14:40:32 +0100

    added TestConfig

--- config.py
+++ config.py
@@ -7,5 +7,9 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
 
+class TestConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'postgresql://currentregister:currentregister@localhost/currentregister')
+
 class ProductionConfig(Config):
     DEBUG = False
